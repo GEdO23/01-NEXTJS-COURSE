f390ebdbb6c1eb69f0754b493da54ee2d012dde4
GEdO23 <rm99632@fiap.com.br>
2023-11-25 16:17:13 -0300

    Preparando o ambiente: Recursos da aula 3 + Componentes do layout.

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import Link from '../src/components/Link';
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <React.Fragment>    
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            
-            
-            `}
-            </style>
-        </React.Fragment>
-    );
-}
 
 export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases</Title>
-            <Link href="/faq">
-                Ir para a página do FAQ
-            </Link>
-        </div>
-    )
+    return <HomeScreen />;
 }
+// import React from 'react';
+// import Link from '../src/components/Link';
+
+// function GlobalStyle() {
+//     return (
+//         <style jsx>{`
+//             body {
+//                 font-family: sans-serif;
+//             }
+        
+//         `}
+//         </style>
+//     )
+// }
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <React.Fragment>    
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+            
+            
+//             `}
+//             </style>
+//         </React.Fragment>
+//     );
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <GlobalStyle/>
+//             <Title as="h1">Alura Cases</Title>
+//             <Link href="/faq">
+//                 Ir para a página do FAQ
+//             </Link>
+//         </div>
+//     )
+// }
